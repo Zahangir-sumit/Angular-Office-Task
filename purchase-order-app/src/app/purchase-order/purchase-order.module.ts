622f57c882228aa190b979db6f81e86681d97fd6
zahangir-sumit <zahangirsumitseu@gmail.com>
2025-11-09 22:28:01 +0600

    date picker

--- purchase-order-app/src/app/purchase-order/purchase-order.module.ts
+++ purchase-order-app/src/app/purchase-order/purchase-order.module.ts
@@ -5,9 +5,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 // Ngx-Bootstrap modules
 import { PaginationModule } from 'ngx-bootstrap/pagination';
-
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { PurchaseOrderListComponent } from './pages/purchase-order-list/purchase-order-list.component';
 import { PurchaseOrderFormComponent } from './pages/purchase-order-form/purchase-order-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
   { path: '', component: PurchaseOrderListComponent },
@@ -26,6 +27,7 @@ const routes: Routes = [
     ReactiveFormsModule,
     FormsModule, // ← This is REQUIRED for ngModel in pagination
     RouterModule.forChild(routes),
+    BsDatepickerModule.forRoot(),
     PaginationModule.forRoot() // ← Use forRoot() here
   ]
 })
